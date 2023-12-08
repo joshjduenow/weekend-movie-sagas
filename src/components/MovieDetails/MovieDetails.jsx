91c9bd1ec7c541e04a9010653229487b53e72702
Joshua Duenow <joshjduenow@gmail.com>
2023-12-08 15:29:16 -0600

    started movie details

---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,17 @@
+import { useHistory } from "react-router-dom";
+
+export default function MovieDetails() {
+
+const history = useHistory()
+
+// const navigateHome () => {
+
+
+// }
+
+return (
+    <button data-testid="toList">⇦Back To Movie List</button>
+)
+
+
+}
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,33 +1,30 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./MovieList.css";
 
 function MovieList() {
-
   const dispatch = useDispatch();
-  const movies = useSelector(store => store.movies);
+  const movies = useSelector((store) => store.movies);
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_MOVIES' });
+    dispatch({ type: "FETCH_MOVIES" });
   }, []);
 
   return (
     <main>
       <h1>MovieList</h1>
       <section className="movies">
-        {movies.map(movie => {
+        {movies.map((movie) => {
           return (
-            <div data-testid='movieItem' key={movie.id}>
+            <div data-testid="movieItem" key={movie.id}>
               <h3>{movie.title}</h3>
-              <img src={movie.poster} alt={movie.title}/>
+              <img src={movie.poster} alt={movie.title} />
             </div>
           );
         })}
       </section>
     </main>
-
   );
 }
-
 
 export default MovieList;
