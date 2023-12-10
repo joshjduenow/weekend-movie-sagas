d1f0389cd7df2205910a26fe3e0f454011422738
Joshua Duenow <joshjduenow@gmail.com>
2023-12-10 14:15:03 -0600

    fixing the details page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,26 +1,40 @@
 import { useHistory } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
-export default function MovieDetails() {
+export default function MovieDetails({}) {
   const history = useHistory();
-
-  const genres = useSelector(store => store.genres);
-  const movieDetails = useSelector(store => store.movieDetails);
+  const id = useParams();
+  const genres = useSelector((store) => store.genres);
+  const movieDetails = useSelector((store) => store.movieDetails);
 
   const navigateHome = () => {
     history.push("/");
   };
+  useEffect(() => {
+    dispatch({
+      type: "GET_DETAILS",
+      payload: id.id,
+    });
+  }, []);
 
   return (
-    <>
-      <button data-testid="toList" onClick={navigateHome}>
-        Back To Movie List
-      </button>
-      <div data-testid="movieDetails">
+    <div data-testid="movieDetails">
+      <div>
+        <img src={movieDetails.poster} />
         <h1>{movieDetails.title}</h1>
-        <img src={movieDetails.poster} alt={movieDetails.title} />
         <p>{movieDetails.description}</p>
+        <ul>
+          {genres.map((genre, i) => (
+            <li key={i}>{genre.category}</li>
+          ))}
+        </ul>
+
+        <button data-testid="toList" onClick={navigateHome}>
+          Home
+        </button>
       </div>
-    </>
+    </div>
   );
 }
