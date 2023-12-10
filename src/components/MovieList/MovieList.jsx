b4d79725796397ab574dd1d2695b713692cefd56
Joshua Duenow <joshjduenow@gmail.com>
2023-12-10 14:26:59 -0600

    cleaned up movie item

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,40 +1,24 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./MovieList.css";
-import { useHistory } from "react-router-dom";
+import MovieItem from "../Movieitem/MovieItem";
 
-function MovieList() {
+export default function MovieList() {
   const dispatch = useDispatch();
-  const history = useHistory();
   const movies = useSelector((store) => store.movies);
 
   useEffect(() => {
     dispatch({ type: "FETCH_MOVIES" });
   }, []);
-  const navigateHome = () => {
-    history.push("/details");
-  };
 
   return (
-    <main>
-      <h1>MovieList</h1>
+    <div>
+      <h1>Movie List</h1>
       <section className="movies">
         {movies.map((movie) => {
-          return (
-            <div data-testid="movieItem" key={movie.id}>
-              <h3>{movie.title}</h3>
-              <img
-                data-testid="toDetails"
-                onClick={navigateHome}
-                src={movie.poster}
-                alt={movie.title}
-              />
-            </div>
-          );
+          return <MovieItem key={movie.id} movie={movie} />;
         })}
       </section>
-    </main>
+    </div>
   );
 }
-
-export default MovieList;
